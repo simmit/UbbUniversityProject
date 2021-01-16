9ba78132009c8256dca9f75a212d44e95a51321f
DENISA-SIMONA MITROI <denisa.mitroi@stud.ubbcluj.ro>
2021-01-17 01:33:25 +0200

    add project

---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using UbbUniversity.Data;
+using UbbUniversity.Models;
+
+namespace UbbUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UbbUniversity.Data.SchoolContext _context;
+
+        public IndexModel(UbbUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Students.ToListAsync();
+        }
+    }
+}
